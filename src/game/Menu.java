c711b18df888431ffa7ffb54ef1f477b1afe3200
paulmorio <schererpaul@rocketmail.com>
2013-03-21 21:55:27 +0000

    Work on Menu

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -7,19 +7,24 @@ import org.newdawn.slick.state.*;
 //this is the menu.
 public class Menu extends BasicGameState {
 
+	Image playNow;
+	Image exitGame;
+
 	public Menu(int State) {
 
 	}
 
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
-
+		playNow = new Image("res/playNow.png");
+		exitGame = new Image("res/exitGame.png");
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		g.fillOval(75,100,100,100);
-		g.drawString("Are you ready?", 80, 70);
+		g.drawString("Are you ready?", 390, 50);
+		playNow.draw(100, 100);
+		exitGame.draw(100, 200);
 
 	}
 
@@ -28,9 +33,16 @@ public class Menu extends BasicGameState {
 		Input input = gc.getInput();
 		int xpos = Mouse.getX();
 		int ypos = Mouse.getX();
-		if ((xpos > 75 && xpos < 175) && (ypos > 160 && ypos <260)){
-			if (input.isMouseButtonDown(0)){
+		//playNow Button
+		if ((xpos > 75 && xpos < 175) && (ypos > 160 && ypos < 260)) {
+			if (input.isMouseButtonDown(0)) {
 				sbg.enterState(1);
+			}
+		}
+		//extitGame button
+		if ((xpos>100 && xpos<311) && (ypos>109 && ypos<160)){
+			if(Mouse.isButtonDown(0)){
+				System.exit(0);
 			}
 		}
 	}
