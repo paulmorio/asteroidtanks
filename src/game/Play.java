16f32a057470957875a85fa7a929ae7d5b471bd1
paulmorio <schererpaul@rocketmail.com>
2013-03-29 02:34:49 +0000

    Trying to work on play

--- src/game/Play.java
+++ src/game/Play.java
@@ -6,16 +6,41 @@ import org.newdawn.slick.state.*;
 
 public class Play extends BasicGameState {
 
-	Image plane;
-	CollidableRenderableObject land;
+	private static final int EMITTER_BOOM = 0;
+	private static final int EMITTER_ASTEROID = 0;
+	private static final int NUM_EMITTERS = 15;
+	private static final int FIRING_DELAY = 120;
+	private boolean soundEnabled = true;
+	private int id;
+	// private Movable player = new Movable(400.0F, 300.0F, new Rectangle());
+
 	float tankx = 400;
 	float tanky = 300;
 	float scale = 1f;
+
+	// asteroids
 	int numberOfAsteroids = 10;
-	CollidableRenderableObject[] asts;
-	Asteroid[] ast = new Asteroid[20];
-	
+	private CollidableRenderableObject[] asts;
+	private Asteroid[] ast = new Asteroid[20];
 	boolean quit = false;
+
+	// Images to be used.
+	private Image tank;
+	private CollidableRenderableObject land;
+	private CollidableRenderableObject bullet;
+
+	// Sounds to be used
+	private Sound soundHit1;
+	private Sound soundHit2;
+	private Sound soundShoot;
+	private Sound soundExplosion;
+	// When we actually have a winning situation
+	// private Sound soundWin;
+
+	// Particles
+	private BaseParticle[] boom = new BaseParticle[15];
+	private BaseParticle[] blood = new BaseParticle[15];
+	private BaseParticle[] barrelboom = new BaseParticle[7];
 
 	public Play(int State) {
 
@@ -24,70 +49,73 @@ public class Play extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 
-		plane = new Image("res/Sherman Tank Sprite.png");
-		land = new CollidableRenderableObject("res/messier81_800x600.jpg",CollidableRenderableObject.Physics.Rectangular);
+		tank = new Image("res/Sherman Tank Sprite.png");
+		land = new CollidableRenderableObject("res/messier81_800x600.jpg",
+				CollidableRenderableObject.Physics.Rectangular);
 		land.invert = true;
 		land.resolve = false;
-		land.SetPosition(0f,0f);
-		//land.position = new Vector2f(2, 5);
+		land.SetPosition(0f, 0f);
+		// land.position = new Vector2f(2, 5);
 
 		for (int i = 0; i < ast.length; i++) {
 			ast[i] = new Asteroid();
 		}
 		asts = new CollidableRenderableObject[numberOfAsteroids];
 		for (int i = 0; i < numberOfAsteroids; i++) {
-			asts[i] = new CollidableRenderableObject("res/asteroid.png",CollidableRenderableObject.Physics.Rectangular,0.8f);
-			asts[i].SetPosition((float)Math.random(), (float)Math.random());
-			asts[i].SetVelocity(RandMinus1To1()*0.3f, RandMinus1To1()*0.1f);
-			asts[i].image.rotate(RandMinus1To1()*180f);
-			//asts[i].Explode();
+			asts[i] = new CollidableRenderableObject("res/asteroid.png",
+					CollidableRenderableObject.Physics.Rectangular, 0.8f);
+			asts[i].SetPosition((float) Math.random(), (float) Math.random());
+			asts[i].SetVelocity(RandMinus1To1() * 0.3f, RandMinus1To1() * 0.1f);
+			asts[i].image.rotate(RandMinus1To1() * 180f);
+			// asts[i].Explode();
 		}
 
 	}
-	public static float RandMinus1To1(){
-		return (float)((Math.random()-0.5)*2f);
+
+	public static float RandMinus1To1() {
+		return (float) ((Math.random() - 0.5) * 2f);
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		if(RenderableObject.rendObjects!=null)
-			for(RenderableObject ro : RenderableObject.rendObjects){
+		if (RenderableObject.rendObjects != null)
+			for (RenderableObject ro : RenderableObject.rendObjects) {
 				ro.Draw();
 			}
-		
-		//for (int i = 0; i < 10; i++) {asts[i].Draw();}
-		//plane.draw(tankx, tanky, scale);
-		
-		//for when the player presses escape
-		if (quit == true){
-			g.drawString("Resume (R)", 390,100);
+
+		// for (int i = 0; i < 10; i++) {asts[i].Draw();}
+		// plane.draw(tankx, tanky, scale);
+
+		// for when the player presses escape
+		if (quit == true) {
+			g.drawString("Resume (R)", 390, 100);
 			g.drawString("Main Menu (M)", 390, 150);
 			g.drawString("Quit Game (Q)", 390, 200);
-			if (quit == false){
+			if (quit == false) {
 				g.clear();
 			}
 		}
 	}
-	
+
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
-		//if(quit)
-			//return;
-		
+		// if(quit)
+		// return;
+
 		Input input = gc.getInput();
 		float del = (float) delta / 1000f;
 		if (input.isKeyDown(Input.KEY_A)) {
-			plane.rotate(-0.2f * delta);
+			tank.rotate(-0.2f * delta);
 		}
 
 		if (input.isKeyDown(Input.KEY_D)) {
-			plane.rotate(0.2f * delta);
+			tank.rotate(0.2f * delta);
 		}
 
 		if (input.isKeyDown(Input.KEY_W)) {
 			float hip = 0.4f * delta;
 
-			float rotation = plane.getRotation();
+			float rotation = tank.getRotation();
 
 			tankx += hip * Math.sin(Math.toRadians(rotation));
 			tanky -= hip * Math.cos(Math.toRadians(rotation));
@@ -96,7 +124,7 @@ public class Play extends BasicGameState {
 		if (input.isKeyDown(Input.KEY_S)) {
 			float hip = 0.4f * delta;
 
-			float rotation = plane.getRotation();
+			float rotation = tank.getRotation();
 
 			tankx -= hip * Math.sin(Math.toRadians(rotation));
 			tanky += hip * Math.cos(Math.toRadians(rotation));
@@ -104,39 +132,39 @@ public class Play extends BasicGameState {
 
 		if (input.isKeyDown(Input.KEY_2)) {
 			scale += (scale >= 5.0f) ? 0 : 0.1f;
-			plane.setCenterOfRotation(plane.getWidth() / 2.0f * scale,
-					plane.getHeight() / 2.0f * scale);
+			tank.setCenterOfRotation(tank.getWidth() / 2.0f * scale,
+					tank.getHeight() / 2.0f * scale);
 		}
 		if (input.isKeyDown(Input.KEY_1)) {
 			scale -= (scale <= 1.0f) ? 0 : 0.1f;
-			plane.setCenterOfRotation(plane.getWidth() / 2.0f * scale,
-					plane.getHeight() / 2.0f * scale);
+			tank.setCenterOfRotation(tank.getWidth() / 2.0f * scale,
+					tank.getHeight() / 2.0f * scale);
 		}
-		
-		//escape to open ingame menu
-		if(input.isKeyDown(Input.KEY_ESCAPE)){
+
+		// escape to open ingame menu
+		if (input.isKeyDown(Input.KEY_ESCAPE)) {
 			quit = true;
 		}
-		
-		//when the player hits escape
+
+		// when the player hits escape
 		if (quit == true) {
-			if (input.isKeyDown(Input.KEY_R)){
+			if (input.isKeyDown(Input.KEY_R)) {
 				quit = false;
 			}
-			
-			if (input.isKeyDown(Input.KEY_M)){
+
+			if (input.isKeyDown(Input.KEY_M)) {
 				sbg.enterState(0);
-				try{
+				try {
 					Thread.sleep(250);
-				}catch(InterruptedException e){
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			
-			if (input.isKeyDown(Input.KEY_Q)){
+
+			if (input.isKeyDown(Input.KEY_Q)) {
 				System.exit(0);
 			}
-			
+
 		}
 
 		// as game runs, moves asteroid across screen
@@ -145,12 +173,12 @@ public class Play extends BasicGameState {
 			ast[i].x += 1;
 			ast[i].y += 1;
 		}
-		
-		if(RenderableObject.rendObjects!=null)
-			for(RenderableObject ro : RenderableObject.rendObjects){
+
+		if (RenderableObject.rendObjects != null)
+			for (RenderableObject ro : RenderableObject.rendObjects) {
 				ro.Update(del);
 			}
-		
+
 		CollidableRenderableObject.CheckCollisions();
 
 	}
